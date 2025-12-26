760169d5ca4ce0486a3f5746209df1c1f7ce430f
its-rohitrawat <rohit98rawat96@gmail.com>
2025-12-26 12:31:45 +0530

    implemented sorting of the list items

--- src/App.js
+++ src/App.js
@@ -70,10 +70,21 @@ function Form({onAddItems}){
 }
 
 function PackingList({items, onDelItem , onToggleItem} ){
+
+    const [sortby, setsortby] = useState("input")
+
+    let sortedItems;
+
+    if(sortby === "input") sortedItems = items;
+
+    if(sortby === "description") sortedItems = items.slice().sort((a,b) => a.description.localeCompare(b.description));
+
+    if(sortby === "packed") sortedItems = items.slice().sort((a,b) => Number(a.packed) - Number(b.packed));
+ 
      return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             key={item.id}
             item={item}
@@ -82,7 +93,20 @@ function PackingList({items, onDelItem , onToggleItem} ){
           />
         ))}
       </ul>
+
+      <div className="actions">
+            <select value={sortby} onChange={(e)=> setsortby(e.target.value)}>
+                <option value='input'>Sort by input order</option>
+                <option value='description'>Sort by description</option>
+                <option value='packed'>Sort by packed status</option>
+            </select>
+            <button>Clear List</button>
+      </div>
+
+
     </div>
+
+   
   );
 }
 
